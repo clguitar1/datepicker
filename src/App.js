67969e9fc6ab9e659c958b04e4a88b813151c369
Charles Loehle <clguitar1@gmail.com>
2020-05-06 14:16:58 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import './App.css';
+import moment from 'moment';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  const handleChange = (date) => setDate(date);
+
+  const m = moment(date, 'YYYY-MM-DD');
+  console.log(m.format('LLLL'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Date Picker</h1>
+      <DatePicker
+        selected={date}
+        onChange={handleChange}
+        showTimeSelect
+        dateFormat='MMMM d, yyyy h:mm aa'
+      />
+      <p>{m.format('LLLL')}</p>
     </div>
   );
 }
